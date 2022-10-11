64a40c547282e82fe5e4bb021c5ef5e337db06c9
sharathpc <sharathchandra0033@gmail.com>
2022-10-11 10:00:10 +0530

    Basepath changes

--- next.config.js
+++ next.config.js
@@ -3,9 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   assetPrefix: process.env.BASE_PATH,
-  publicRuntimeConfig: {
-    basePath: process.env.BASE_PATH,
-  },
+  basePath: process.env.BASE_PATH,
 }
 
 module.exports = nextConfig
